5aa01278f4be78eed196e30d20097f5ba9cda223
Tariq <csetariq@gmail.com>
2016-08-29 10:05:24 +0530

    intermediate commit

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -1,8 +1,37 @@
 package com.test;
 
-public class App {
-    public static void main(String... args) {
-        System.out.println("Hello, World!");
-    }
-}
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import com.mustang.utf16toutf8.io.DualByteFetchInputStream;
+
+public class App {
+	public static void main(String... args) {
+		try {
+			FileOutputStream fileOut = new FileOutputStream(new File("test.txt"));
+			BufferedOutputStream bufFileOut = new BufferedOutputStream(fileOut);
+			toUTF8(System.in, bufFileOut);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
+	}
+
+	private static void toUTF8(InputStream in, OutputStream out) throws IOException {
+		try (DualByteFetchInputStream dByteIn = new DualByteFetchInputStream(in);) {
+
+			int temp;
+			while ((temp = dByteIn.read()) != -1) {
+				
+			}
+		}
+	}
+}
