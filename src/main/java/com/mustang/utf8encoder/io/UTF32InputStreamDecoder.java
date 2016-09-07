7f536189f7afd87993d203dbd20aca78e02b0018
Tariq <csetariq@gmail.com>
2016-09-07 14:26:10 +0530

    refactor the read()

--- src/main/java/com/mustang/utf8encoder/io/UTF32InputStreamDecoder.java
+++ src/main/java/com/mustang/utf8encoder/io/UTF32InputStreamDecoder.java
@@ -3,31 +3,49 @@ package com.mustang.utf8encoder.io;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * A decoder that reads characters from a UTF-32 encoded input stream * 
+ * 
+ * @author Tariq
+ *
+ */
 public class UTF32InputStreamDecoder extends UTFInputStreamDecoder {
 
-    public UTF32InputStreamDecoder(InputStream in, int size) throws IOException {
+    public UTF32InputStreamDecoder(InputStream in, int size) {
         super(in, size);
     }
 
-    public UTF32InputStreamDecoder(InputStream in) throws IOException {
+    public UTF32InputStreamDecoder(InputStream in) {
         this(in, 8192);
     }
 
+    /**
+     * In UTF-32, each character's Unicode code-point is encoded in 4 bytes
+     * 
+     * It reads 4 bytes at once and returns the code-point of one character
+     * 
+     * @return  code-point of the next character</br>
+     *          or</br>
+     *          <code>-1 if end-of-stream is reached</code>
+     *          
+     * @exception   IOException if the stream is closed or if I/O error occurs
+     */
     @Override
-    public int read() throws IOException {
+    public synchronized int read() throws IOException {
         
+        final int NUM_BYTES_UTF_32 = Integer.BYTES;
         int utf32Word = 0;
         
-        for (int i = 3; i >= 0; --i) {
+        for (int i = 0; i < NUM_BYTES_UTF_32; ++i) {
             int temp = stream.read();
             
-            if (i == 3 && temp == -1)
+            if (i == 0 && temp == -1)
                 return temp;
             
-            if (i != 3 && temp == -1)
+            if (i > 0 && temp == -1)
                 throw new IllegalArgumentException("Invalid UTF-32 input stream");
             
-            utf32Word |= (temp & 0xff) << (i * 8);
+            utf32Word |= (temp & 0xff) << ((NUM_BYTES_UTF_32 - 1 - i) * Byte.SIZE);
         }
         
         return utf32Word;
